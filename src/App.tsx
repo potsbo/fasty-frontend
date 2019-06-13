13e9aad0c85d928f1ee62f95e2b0deb4011ca3ed
kdnk <kodai.nakamura.215@gmail.com>
2019-06-13 20:14:10 +0900

    npx create-react-app . --typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
-import React, { Component } from "react";
-import { Provider } from "react-redux";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-import { configureStore } from "./store/configureStore";
-
-const store = configureStore();
-
-class App extends Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <div>aa</div>
-      </Provider>
-    );
-  }
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
