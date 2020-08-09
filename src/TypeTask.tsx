c43b90d13667d22771a2d65c4e817ed0d7afc1b3
Shimpei Otsubo <potsbo@gmail.com>
2020-08-09 17:01:46 +0900

    Check correct key down

--- src/TypeTask.tsx
+++ src/TypeTask.tsx
@@ -7,18 +7,28 @@ interface Props {
 
 
 const TypeTask = (props: Props) => {
+    const valid = (typed: string) => {
+        return props.sentence.startsWith(typed)
+    }
     const [typed, setTyped] = useState("")
+    const [missTyped, setMissTyped] = useState("")
 
     const onKeyDown = (event: KeyboardEvent) => {
         setTyped((current: string) => {
-            return current += event.key
+            const challenge = current + event.key
+            if (valid(challenge)) {
+                setMissTyped("")
+                return challenge
+            }
+
+            setMissTyped((current: string) => {
+                return current + event.key
+            })
+            return current
         })
     }
 
-    console.log("hello")
-
     useEffect(() => {
-        console.log("event listener")
         window.addEventListener("keydown", onKeyDown)
     }, [props])
     return (
@@ -27,7 +37,12 @@ const TypeTask = (props: Props) => {
                 {props.sentence}
             </div>
             <div>
-                {typed}
+                <span>
+                    {typed}
+                </span>
+                <span style={{ color: 'red' }}>
+                    {missTyped}
+                </span>
             </div>
         </div>
     );
