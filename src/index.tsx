de1654e90067f2e558746eecf9e34959240987da
Shimpei Otsubo <potsbo@gmail.com>
2020-08-10 02:13:45 +0900

    Not to use store

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
 
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { store } from "./store/configureStore";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <App />,
   document.getElementById("root")
 );
 
