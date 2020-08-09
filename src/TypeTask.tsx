7f369099bf2b4cbe6201b3735e797c2a4ec4fc31
Shimpei Otsubo <potsbo@gmail.com>
2020-08-09 16:54:43 +0900

    Allow typing

--- src/TypeTask.tsx
+++ src/TypeTask.tsx
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 interface Props {
     sentence: string
 }
 
 
+
 const TypeTask = (props: Props) => {
+    const [typed, setTyped] = useState("")
+
+    const onKeyDown = (event: KeyboardEvent) => {
+        setTyped((current: string) => {
+            return current += event.key
+        })
+    }
+
+    console.log("hello")
+
+    useEffect(() => {
+        console.log("event listener")
+        window.addEventListener("keydown", onKeyDown)
+    }, [props])
     return (
         <div>
-            {props.sentence}
+            <div>
+                {props.sentence}
+            </div>
+            <div>
+                {typed}
+            </div>
         </div>
     );
 };
