15063e8b58faa0c0b7e2b0634d869aba8b6c8818
Shimpei Otsubo <potsbo@gmail.com>
2020-08-10 16:56:27 +0900

    Remove unused func

--- src/Lesson.tsx
+++ src/Lesson.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import TypeTask, { State } from "./TypeTask";
 import { Course } from "./courses"
 import {
