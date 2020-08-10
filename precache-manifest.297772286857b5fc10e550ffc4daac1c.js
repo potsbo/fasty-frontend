8224013d3fe8a3c83b70a3fa1042007549a18d02
potsbo <potsbo@users.noreply.github.com>
2020-08-10 09:03:48 +0000

    deploy: 5d23ff429ce6785cca0015ef4f729681a5e2b4f3

---
+++ precache-manifest.297772286857b5fc10e550ffc4daac1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f827ab84d108d84f00a8e188dca25d2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78b0930ad0f0e3a73e09",
+    "url": "/static/js/2.6d5acdde.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6d5acdde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "542e42776806ff867fab",
+    "url": "/static/js/main.bcb865d4.chunk.js"
+  },
+  {
+    "revision": "abe816aad07b5660e4fc",
+    "url": "/static/js/runtime-main.973ecbc6.js"
+  }
+]);
