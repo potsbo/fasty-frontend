7137a4a88266d7d1a8b29516640764d9c7303771
Shimpei Otsubo <potsbo@gmail.com>
2020-08-10 14:32:39 +0900

    Configure react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,38 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import CourseView from "./CourseView"
 import abcd from "./courses"
 
 const App: React.FC = () => {
-  return <CourseView course={abcd} />
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/courses/abcd">A Basic Course in Dvorak</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+        renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/courses/abcd">
+            <CourseView course={abcd} />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
