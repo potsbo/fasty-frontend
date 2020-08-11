020e2b4f63121a8211cd0c66b82a03dd5b8faf88
Shimpei Otsubo <potsbo@gmail.com>
2020-08-12 03:11:31 +0900

    Add more test cases

--- src/Keyboard.test.tsx
+++ src/Keyboard.test.tsx
@@ -7,3 +7,19 @@ it("converts from qwerty to dvorak", () => {
   expect(convert("s")).toBe("o");
   expect(convert("q")).toBe("'");
 });
+
+it("converts from dvorak to left handed dvorak", () => {
+  const convert = getLayoutConverter(LayoutName.Dvorak, LayoutName.LDvorak);
+  expect(convert("a")).toBe("-");
+  expect(convert("m")).toBe("i");
+  expect(convert("s")).toBe("8");
+  expect(convert("q")).toBe("x");
+});
+
+it("converts from dvorak to right handed dvorak", () => {
+  const convert = getLayoutConverter(LayoutName.Dvorak, LayoutName.RDvorak);
+  expect(convert("a")).toBe("7");
+  expect(convert("m")).toBe("w");
+  expect(convert("s")).toBe("k");
+  expect(convert("q")).toBe("0");
+});
